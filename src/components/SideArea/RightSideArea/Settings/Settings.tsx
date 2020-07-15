773dfdb9e203394417a0fa3750db369d10005e3c
mnaumann <michael.naumann@etecture.de>
2020-07-15 09:40:28 +0200

    Change Settings style and elevated on z-index

--- src/components/SideArea/RightSideArea/Settings/Settings.tsx
+++ src/components/SideArea/RightSideArea/Settings/Settings.tsx
@@ -1,4 +1,4 @@
-import React, {ClassAttributes, useEffect, useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 import styles from './Settings.module.css';
 
 export interface ISettingsProps {
@@ -42,13 +42,17 @@ function Settings(props: ISettingsProps) {
       <div className={styles.header}/>
       <section className={styles.option}>
         <p className={styles.optionName}>Scale:</p>
-        <input className={styles.slider} type="range" min="0.5" max="2" step="0.1" onChange={changeScaleHandler} ref={scaleSlider}/>
-        <input className={styles.numberInput} type="text" onChange={changeScaleHandler} ref={scaleInput}/>
+        <section className={styles.optionConfig}>
+          <input className={styles.slider} type="range" min="0.5" max="2" step="0.1" onChange={changeScaleHandler} ref={scaleSlider}/>
+          <input className={styles.numberInput} type="text" onChange={changeScaleHandler} ref={scaleInput}/>
+        </section>
       </section>
       <section className={styles.option}>
         <p className={styles.optionName}>Background:</p>
-        <div className={styles.bgButtonDark} onClick={changeBackgroundDarkHandler} ref={bgButtonDark}/>
-        <div className={styles.bgButtonLight} onClick={changeBackgroundLightHandler} ref={bgButtonLight}/>
+        <section className={styles.optionConfig}>
+          <div className={styles.bgButtonDark} onClick={changeBackgroundDarkHandler} ref={bgButtonDark}/>
+          <div className={styles.bgButtonLight} onClick={changeBackgroundLightHandler} ref={bgButtonLight}/>
+        </section>
       </section>
       <div className={styles.buttonFooter}>
         <div className={styles.safe} onClick={props.hideSettings}>Close</div>
