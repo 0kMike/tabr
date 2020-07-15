9d04de1d638703982e9a492f07bb74d64480bed4
mnaumann <michael.naumann@etecture.de>
2020-07-15 15:20:58 +0200

    Adjust Style for newBookmark component

--- src/components/NewBookmark/NewBookmark.tsx
+++ src/components/NewBookmark/NewBookmark.tsx
@@ -12,13 +12,33 @@ function NewBookmark(props: INewBookmarkProps) {
   }
 
   return (
-    <div className={styles.newBookmark}>
-      <h1 className={styles.header}>Add Bookmark</h1>
-      <div className={styles.buttonFooter}>
-        <div className={styles.cancelButton} onClick={cancelClickHandler}>Cancel</div>
-        <div className={styles.confirmButton}>Confirm</div>
+    <div className={styles.newBookmarkBackground}>
+      <div className={styles.newBookmark}>
+        <h1 className={styles.header}>Add Bookmark</h1>
+        <form className={styles.form}>
+          <label>
+            Display Name:
+            <br/>
+            <input type="text" name="name" className={styles.inputText}/>
+          </label>
+          <label>
+            Link:
+            <br/>
+            <input type="text" name="link" className={styles.inputText}/>
+          </label>
+          <label>
+            Image:
+            <br/>
+            <input type="file" name="thumbnail"/>
+          </label>
+          <div className={styles.buttonFooter}>
+            <div className={styles.submitButton} onClick={cancelClickHandler}>Cancel</div>
+            <input className={styles.submitButton} type="submit" value="Submit"/>
+          </div>
+        </form>
       </div>
     </div>
+
   )
 }
 
